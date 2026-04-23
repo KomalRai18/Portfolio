9910c5910d1371307f5a74381ea8691b91a44b16
Komal Rai <99144267+KomalRai18@users.noreply.github.com>
2026-04-23 17:14:34 +0500

    fix: remove certificate section from portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Skills />
         <Experience />
         <Projects />
-        <Certificates />
+        {/* <Certificates /> */}
         <GithubSection />
         <Contact />
       </main>
